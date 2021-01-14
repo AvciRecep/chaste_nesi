874c1b2dd7ca3a6295baf9eb1504d600a8b2a417
Callum Walley <callum.walley@nesi.org.nz>
2021-01-14 14:59:35 +1300

    mmm

--- rebuild.sh
+++ rebuild.sh
@@ -7,8 +7,16 @@ module load Singularity
 
 if [[ $# -lt 1 ]];then echo "Not enough args"; exit 1;fi
 
+#for arg in $@; do 
+
+#f [[ -f $arg]]; do
+
+# stat -c "%Y" $arg
+
+
 wosif=$(basename ${1%.*})
 echo $wosif
+exit 
 rm -f ".${1}"
 singularity build ".${1}"  "shub://nesi/${wosif/_/:}"
 rm -f "${1}"
